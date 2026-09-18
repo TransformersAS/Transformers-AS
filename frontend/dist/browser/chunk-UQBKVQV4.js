ed4541d02a632326f3afa117d193a66557f8127f
Pacho73G <franciscoguzmanv11@gmail.com>
2026-09-18 12:47:53 -0500

    chore: optimizar repositorio e incluir decisiones arquitectonicas

--- frontend/dist/browser/chunk-UQBKVQV4.js
+++
@@ -1,1 +0,0 @@
-import{a,b}from"./chunk-NHGBHXHN.js";import"./chunk-TDXZVYRX.js";import"./chunk-KWNNEOZ6.js";import"./chunk-3KUMRP2G.js";import"./chunk-NMFL75IO.js";import"./chunk-4CLCTAJ7.js";export{b as iosTransitionAnimation,a as shadow};
